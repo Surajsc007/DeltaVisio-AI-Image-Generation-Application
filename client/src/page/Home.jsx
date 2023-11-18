ab7302e743bef5f56250e39544aeb3e3df02a7b5
Surajsc007 <surajsc983@gmail.com>
2023-11-19 00:41:14 +0530

    update

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://delta-visio-ai-image-generation-application.vercel.app/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
